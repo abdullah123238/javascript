94183adb7371cdb11736b85bf8f7720b78394421
abdullah <adeosunabdullah6@gmail.com>
2025-03-12 14:16:15 +0100

    my first com

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -222,3 +222,235 @@ getUser(1)
     .catch((err) => {
         console.error(err);
     });
+
+    //Promise example 2:
+    const register = new Promise ((resolve, reject) => {
+
+        let success = false;
+        setTimeout(() => {
+            if(success){
+                resolve("The operation is successful")
+            }else{
+                reject("The operation Failed")
+            }
+        }, 2000);
+    });
+
+    register.then((positiveResult) => {
+        console.log({resolve: positiveResult})
+    }).catch((negativeResult) => {
+        console.log({reject: negativeResult})
+    }).finally(() =>{
+        console.log("The operation completed")
+    });
+
+
+    // Asyn/Await: It simplifies the synchronous handling process
+
+    //Creating a promise
+    const fetchDate = () => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve("Data Fecthed")
+            }, 2000)
+        })
+    }
+
+    //Async Function
+    const handleFetchedData = async () => {
+        const data = await handleFetchedData()
+        console.log({result: data})
+    }
+
+    handleFetchedData()
+
+
+    // EXAMPLE 2
+
+  const cohort = {name: "Muse",height: "77", skin:" fair"}
+  
+  const fetchCohortData = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(cohort)
+      }, 3000)
+    })
+  }
+
+  const processCohortData = (data) =>{
+    return new Promise ((resolve) => {
+      setTimeout(() => {
+        console.log(data)
+        resolve(data)
+      }, 4000)
+    })
+  }
+  const savedCohortData = (processedData) =>{
+    return new Promise ((resolve) => {
+      setTimeout(() => {
+  
+        resolve(processedData)
+      }, 5000)
+    })
+  }
+
+  const handleCohortData = async () =>{
+    console.log("Starting point");
+
+    const data = await fetchCohortData();
+    console.log({step1: data});
+
+    let processedData
+    const getProcessData = await processCohortData(data);
+
+    if(getProcessData){
+      const numHeight = Number(getProcessData.height);
+      const newResult = {...getProcessData, height: numHeight}
+
+      processedData = newResult;
+    }
+    console.log({step2: processedData});
+
+    const savedData = await savedCohortData(processedData);
+    console.log({step3: savedData});
+
+  }
+
+  handleCohortData();
+
+  //Error handling in async/Await
+  const fetchUser = () => {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve("User fetched")
+        }, 5000);
+    })
+  }
+  const fetchPost = () => {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve("Post fetched Successfully")
+        }, 5000);
+    })
+  }
+
+  const handleUserAndPost = async () => {
+      
+    try {
+        console.log("Fetching user and posts")
+        const [user, posts] = await Promise.all([fetchUser(), fetchPost()])
+        console.log(`User: ${user}`) 
+        console.log(`Posts: ${posts}`) 
+    } catch (error) {
+        console.log(`Error: ${Error}`)
+    }
+  }
+  handleUserAndPost()
+
+  
+// class work
+
+//Inscructions
+
+// 1. Create  function to simulate fetching user data from a database (use setTimeout)
+// 2. Create another function to simulate fetching user tasks from a database
+// 3. Use async/await with try..catch to handle errors
+// 4. Simulate a case where the user is not found, and another where the tasks fail to load
+
+//Correction 
+
+
+const users = {
+    1: {id: 1, name: "Soliu", skin: "Dark"},
+    2: {id: 2, name: "Ridwan", skin: "Light"},
+    3: {id: 3, name: "Feranmi", skin: "Dark"}
+  
+  }
+  
+  
+  const getTheUser =  (userID) => {
+    return new Promise((resolve, reject) => {
+  
+      setTimeout(() => {
+  
+      
+  
+  
+        
+        if (users[userID]) {
+  
+          resolve(users[userID])
+          
+        }else {
+  
+          reject(new Error("User not available in the database"))
+        }
+  
+        
+      }, 2000);
+    })
+  }
+  
+  
+  const getUserTask =  (userID) => {
+    return new Promise((resolve, reject) => {
+  
+      setTimeout(() => {
+  
+        const tasks = {
+          1: ["Dance", "Give up"],
+          2: ["Cook", "Break fast"],
+          3: ["Attend meeting"]
+  
+        }
+  
+  
+        
+        if (users[userID] && tasks[userID])  {
+  
+          resolve(tasks[userID])
+          
+        }else {
+  
+          reject(new Error("User with this task is not available in the database"))
+        }
+  
+        
+      }, 2000);
+    })
+  }
+  
+  
+  const getUserAndTask = async (userID) => {
+    
+  try {
+  
+    console.log("Fetching user....")
+  
+    const user = await getTheUser(userID);
+  
+    console.log({user});
+  
+  
+  
+    console.log(`Fetching ${user?.name}'s task`);
+  
+    const userTask = await getUserTask(userID);
+  
+    console.log(`${user?.name}'s has this task: ${userTask}`);
+    
+  } catch (error) {
+  
+    console.log(`Error : ${error}`)
+    
+  }finally {
+  
+    console.log("Operation completed")
+  }
+  
+  }
+  
+  
+  
+  getUserAndTask("4")
+  
